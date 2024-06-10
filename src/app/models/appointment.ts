0eb28a0f33fd4a7c1a7c9dc091ea5c04f70ab396
lokesh-sangem <vinaysangem2001@gmail.com>
2024-06-10 15:08:58 +0530

    add project

---
+++ src/app/models/appointment.ts
@@ -0,0 +1,10 @@
+export interface Appointment {
+    id:number,
+    title:string,
+    date:Date,
+    
+    
+}
+
+
+
---
+++ src/app/models/dummy-appointment.ts
@@ -0,0 +1,6 @@
+export interface DummyAppointment {
+
+    id:number,
+  title:string,
+    date:Date,
+}
